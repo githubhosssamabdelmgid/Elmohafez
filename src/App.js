7fd928ffd912636fb3b748c101e585916bbdd721
hossam ahmed <githubhosssamahmed>
2023-03-24 18:35:09 +0300

    for you

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './Nav';
+import Services from './Services';
+import About from './About';
+import Blog from './Blog';
+import Client from './Client';
+import Contact from './Contact';
+import Copyright from './Copyright';
+import Footer from './Footer';
+import Newslater from './Newslater';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+              <Nav/>
+      <Services/>
+      <About/>
+      <Blog/>
+      <Client/>
+      <Newslater/>
+      <Contact/>
+      <Footer/>
+      <Copyright/>
+
     </div>
   );
 }
